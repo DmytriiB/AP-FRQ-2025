3b337aa259312a5d318c7786a2a304e4c15f5eb9
DmytriiB <dmytriib@nycstudents.net>
2025-05-12 10:09:12 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,5 +1,12 @@
 public int walkDogs(int hour){
         //find out from dogWalkCompany how many dogs at that hour
         int numDogs =  numAvailableDogs(hour);
-        if()
+        if(maxDogs > numDogs){
+            company.updateDogs(numDogs);
+            return numDogs;
+        }
+        else{
+            company.updateDogs(maxDogs);
+            return maxDogs;
+        }
     }

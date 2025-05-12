c54ea99b5ce1f444da6151906cf997a440e33c35
DmytriiB <dmytriib@nycstudents.net>
2025-05-12 09:58:35 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,3 +1,5 @@
 public int walkDogs(int hour){
-        
-  }    
+        //find out from dogWalkCompany how many dogs at that hour
+        int numDogs =  numAvailableDogs(hour);
+        if()
+    }

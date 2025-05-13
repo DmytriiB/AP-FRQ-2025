e2a759861a2ded8a9ff3514c54f6ef80c941ad7f
DmytriiB <dmytriib@nycstudents.net>
2025-05-13 10:06:18 -0400

    Update SignedText.java

--- Q2/SignedText.java
+++ Q2/SignedText.java
@@ -1,2 +1,34 @@
 public class SignedText{
+    private String firstName;
+    private String lastName;
+
+    public SignedText(String fn, String ln){
+        firstName = fn;
+        lastName = ln;
+    }
+
+    public String getSignature(){
+        if(firstName.length()==0){
+            return lastName;
+        }
+        else{
+            return firstName.substring(0,1)+"-"+lastName;
+        }
+    }
+
+    public String addSignature(String str){
+        String sig = getSignature();
+        int sigLen = sig.length();
+
+        if(str.idexOf(sig) == str.length() - sigLen && str.length() >= sigLen){
+            return str;
+        }
+        else if(str.indexOf(sig) == 0){
+            return str.substring(sigLen) + sig;
+        }
+        else{
+            return str + sig;
+        }
+    }
+}
   
